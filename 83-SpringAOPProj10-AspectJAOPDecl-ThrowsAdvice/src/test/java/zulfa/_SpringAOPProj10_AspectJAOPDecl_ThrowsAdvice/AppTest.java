41a807ad6722db54befc009c9efd77f51958e019
tl <HP@DESKTOP-PDMUKAP>
2020-07-24 20:33:56 +0530

    aspectj AOP

---
+++ 83-SpringAOPProj10-AspectJAOPDecl-ThrowsAdvice/src/test/java/zulfa/_SpringAOPProj10_AspectJAOPDecl_ThrowsAdvice/AppTest.java
@@ -0,0 +1,20 @@
+package zulfa._SpringAOPProj10_AspectJAOPDecl_ThrowsAdvice;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
